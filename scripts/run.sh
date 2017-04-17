fbf1cc9a43635af2fa4d70e4e56300671245b3f8
Dion Woolley <woolley.dion@gmail.com>
2017-04-17 12:59:57 +1200

    Update scripts I hope, this works

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -e
 
 cd /home/ubuntu/concierge
 tmux new-session -d -s concierge '/usr/bin/node /home/ubuntu/concierge/main.js --debug --log --timestamp test'
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
+set -e
 
 tmux kill-session -t concierge || true
